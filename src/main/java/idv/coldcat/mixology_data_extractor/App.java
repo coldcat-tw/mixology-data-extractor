ce7007b07e47494ad8795ea547dd3d3cb9cfa210
coldcat-tw <coldcat.tw@gmail.com>
2021-05-31 04:07:08 +0800

    Implement function to collect recipe data & save to JSON file

--- src/main/java/idv/coldcat/mixology_data_extractor/App.java
+++ src/main/java/idv/coldcat/mixology_data_extractor/App.java
@@ -6,7 +6,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -20,9 +23,12 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+
+import idv.coldcat.mixology_data_extractor.RecipeDataCollector.RecipeData;
 
 public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
@@ -53,6 +59,7 @@ public class App {
 	private void collectRecipeLinks(String targetPath) throws IOException {
 		Injector injector = Guice.createInjector(new ConfigurationModule("mixology-data-extractor"));
 		RecipeUrlCollector recipeUrlCollector = injector.getInstance(RecipeUrlCollector.class);
+		
 		String recipeListLinksFilePath = targetPath + "/output/recipeListLinks.txt";
 		Path path = Paths.get(recipeListLinksFilePath);
 		boolean exists = Files.exists(path);
@@ -73,21 +80,53 @@ public class App {
 			logger.info(recipeLinks.toString());
 		}
 	}
-
-	private void outputResult(String targetPath, Set<String> result) throws IOException {
-		if (!Strings.isNullOrEmpty(targetPath)) {
-			final File f = new File(targetPath + "/output/recipeLinks.txt");
-			StringBuffer sb = new StringBuffer();
-			for (String s : result) {
-				sb.append(s).append("\n");
+	
+	private void collectRecipeData(String targetPath) throws IOException {
+		Injector injector = Guice.createInjector(new ConfigurationModule("mixology-data-extractor"));
+		RecipeDataCollector recipeDataCollector = injector.getInstance(RecipeDataCollector.class);
+		
+		String recipeLinksFilePath = targetPath + "/output/recipeLinks.txt";
+		Path path = Paths.get(recipeLinksFilePath);
+		boolean exists = Files.exists(path);
+		logger.info("recipeLinksFilePath;" + recipeLinksFilePath + ";exists;" + exists);
+		if (exists) {
+			String content = FileUtils.readFileToString(path.toFile(), StandardCharsets.UTF_8);
+			List<RecipeData> recipeDataList = new ArrayList<>();
+			for (String line : content.split("\n")) {
+				logger.info("processing " + line);
+				RecipeData data = recipeDataCollector.collect(line);
+				recipeDataList.add(data);
+				randomSleep();
 			}
-			FileUtils.writeStringToFile(f, sb.toString().trim(), StandardCharsets.UTF_8);
+			logger.info(targetPath);
+			Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+			for (RecipeData data : recipeDataList) {
+				String s = gson.toJson(data);
+				logger.info(s);
+				String outputFilePath = targetPath + "/output/" + data.getId() + ".json";
+				List<String> list = new ArrayList<>();
+				list.add(s);
+				outputResult(outputFilePath, list);
+			}
+
+			logger.info(recipeDataList.toString());
 		}
 	}
 
-	private void randomSleep() {
-		int upper = 5;
-		int lower = 2;
+	private void outputResult(String targetPath, Collection<String> result) throws IOException {
+		final File f = new File(targetPath);
+		StringBuffer sb = new StringBuffer();
+		for (String s : result) {
+			sb.append(s).append("\n");
+		}
+		FileUtils.writeStringToFile(f, sb.toString().trim(), StandardCharsets.UTF_8);
+	}
+	
+	private int randomSleep() {
+		return randomSleep(6, 2);
+	}
+	
+	private int randomSleep(int upper, int lower) {
 		Random random = new Random();
 		int randomNumber = random.nextInt(upper - lower) + lower;
 		try {
@@ -97,6 +136,7 @@ public class App {
 			Thread.currentThread().interrupt();
 			e.printStackTrace();
 		}
+		return randomNumber;
 	}
 
 	public static void main(String[] args) {
@@ -121,6 +161,9 @@ public class App {
 					break;
 				case "get_recipe_link":
 					app.collectRecipeLinks(targetPath);
+					break;
+				case "get_recipe_data":
+					app.collectRecipeData(targetPath);
 					break;
 				default:
 					break;
---
+++ src/main/java/idv/coldcat/mixology_data_extractor/RecipeDataCollector.java
@@ -0,0 +1,162 @@
+package idv.coldcat.mixology_data_extractor;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecipeDataCollector {
+	private static final Logger logger = LoggerFactory.getLogger(RecipeDataCollector.class);
+
+	@Inject
+	@Named("DEFAULT_UA")
+	private String ua = null;
+
+	public RecipeData collect(String url) throws IOException {
+		Document doc = Jsoup.connect(url).userAgent(this.ua).get();
+		RecipeData data = new RecipeData();
+		processId(url, data);
+		processName(doc, data);
+		processEngName(doc, data);
+		processIntroduction(doc, data);
+		processIngredientsAndSteps(doc, data);
+		return data;
+	}
+
+	private void processIngredientsAndSteps(Document doc, RecipeData data) {
+		Elements ingredientsElem = doc.getElementsByClass("recipe_detail_ingred");
+		for (Element elem : ingredientsElem) {
+			Elements detailContentElements = elem.getElementsByClass("recipe_detail_content");
+			if (detailContentElements.size() > 0) {
+				Elements notThElements = elem.select("tr:not(:has(th))");
+				Elements filteredElements = notThElements.not("tr:last-child");
+				String unescapeEntities = filteredElements.text().replaceAll("\\&nbsp", "");
+				data.setIngredients(unescapeEntities);
+				
+				// process steps
+				processSteps(data, detailContentElements);
+			}
+		}
+	}
+
+	private void processSteps(RecipeData data, Elements detailContentElements) {
+		Elements stepsElements = detailContentElements.select("div:first-child");
+		List<TextNode> stepsContentList = stepsElements.textNodes();
+		StringBuffer sb2 = new StringBuffer();
+		for (TextNode s : stepsContentList) {
+			sb2.append(s.text()).append("\n");
+		}
+		data.setSteps(sb2.toString().trim());
+	}
+
+	private void processIntroduction(Document doc, RecipeData data) {
+		Element introElem = doc.getElementById("CT1_RecipeIntro");
+		if (introElem != null) {
+			data.setIntro(introElem.wholeText());
+		}
+	}
+
+	private void processEngName(Document doc, RecipeData data) {
+		Element engNameElem = doc.getElementById("CT1_RecipeEName");
+		if (engNameElem != null) {
+			data.setEngName(engNameElem.ownText());
+		}
+	}
+
+	private void processName(Document doc, RecipeData data) {
+		Element nameElem = doc.getElementById("CT1_RecipeCName");
+		if (nameElem != null) {
+			data.setName(nameElem.ownText());
+		}
+	}
+
+	private void processId(String url, RecipeData data) {
+		String[] split = url.split("=");
+		String id = split[1];
+		data.setId(id);
+	}
+	
+	public class RecipeData {
+		private String id;
+		private String name;
+		private String engName;
+		private String intro;
+		private String ingredients;
+		private String steps;
+
+		public RecipeData() {}
+		
+		public RecipeData(String id, String name, String engName, String intro, String ingredients, String steps) {
+			this.setId(id);
+			this.name = name;
+			this.engName = engName;
+			this.intro = intro;
+			this.ingredients = ingredients;
+			this.steps = steps;
+		}
+		
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEngName() {
+			return engName;
+		}
+
+		public void setEngName(String engName) {
+			this.engName = engName;
+		}
+
+		public String getIntro() {
+			return intro;
+		}
+
+		public void setIntro(String intro) {
+			this.intro = intro;
+		}
+
+		public String getIngredients() {
+			return ingredients;
+		}
+
+		public void setIngredients(String ingredients) {
+			this.ingredients = ingredients;
+		}
+
+		public String getSteps() {
+			return steps;
+		}
+
+		public void setSteps(String steps) {
+			this.steps = steps;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public String toString() {
+			return "RecipeData [id=" + id + ", name=" + name + ", engName=" + engName + ", intro=" + intro
+					+ ", ingredients=" + ingredients + ", steps=" + steps + "]";
+		}
+
+	}
+}
